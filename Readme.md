942ee2410152671710d549f3fda12dd3a2cbf9f2
Francois Seguin <francoisseguin@Francoiss-MacBook-Air.local>
2020-01-16 14:27:06 -0500

    adding mac support

--- Readme.md
+++ Readme.md
@@ -1,14 +1,18 @@
 # music-organizer
 
-## commands
-
-- `count` -> return playlist name: playlist track count: playlist description
-- `find whatever` -> find of whatever matches a playlist track. return playlist name: list of track that matches for each playlist
-
 ## development
 
 - `nodemon --exec "bsh test.sh" ./music.py`
 
-## tools
+## installation
 
-- gmusicapi
+- `pip3 install gmusicapi`
+- add this folder to your environment variable path
+
+## command 
+
+ command | description
+ -------  | ------------
+ login     |  register this pc
+ count   | list all playlists with songs count and tags
+ find "query" | find query in all playlist
--- music.py
+++ music.py
@@ -1,53 +1,68 @@
-#! python
-
 from gmusicapi import Mobileclient
 import sys
 
 collection = Mobileclient()
 
-# collection.perform_oauth()
-collection.oauth_login(collection.FROM_MAC_ADDRESS)
+try: 
+    sys.argv[1]
+except:
+    print("please provide an argument: login, count, find")
+    sys.exit()
 
-allplaylist = collection.get_all_user_playlist_contents()
-allplaylist.sort(key=lambda e: e['name'], reverse=True)
-allplaylist = allplaylist[:-2]
+if sys.argv[1] == "login":
+    collection.perform_oauth()
+
+try: 
+    collection.oauth_login(collection.FROM_MAC_ADDRESS)
+except:
+    print("Not loggin. Try the following command: music login")
+else: 
+
+    allplaylist = collection.get_all_user_playlist_contents()
+    allplaylist.sort(key=lambda e: e['name'], reverse=False)
+    allplaylist.pop(0)
 
 
-def encode_utf(text):
-    return text.encode('utf-8').strip()
+    def encode_utf(text):
+        return text.encode('utf-8').strip()
+
+    # if sys.argv[1] == "logout":
+    #     try: 
+    #         collection.deauthorize_device(collection.FROM_MAC_ADDRESS)
+    #     finally:     
+    #         sys.exit()
+
+    if sys.argv[1] == "count":
+        for playlist in allplaylist:
+            # name = encode_utf(playlist['name'])
+            print("Playlist name: {} Count: {} Description: {}".format(
+                playlist['name'], len(playlist['tracks']), playlist["description"]))
 
 
-if sys.argv[1] == "count":
-    for playlist in allplaylist:
-        name = encode_utf(playlist['name'])
-        print("Playlist name: {} Count: {} Description: {}".format(
-            name, len(playlist['tracks']), playlist["description"]))
+    if sys.argv[1] == "find":
 
+        search_query = sys.argv[2]
+        result = {}
 
-if sys.argv[1] == "find":
+        for playlist in allplaylist:
 
-    search_query = sys.argv[2]
-    result = {}
+            playlist_name = playlist['name'].encode('utf-8').strip()
+            song_list = []
 
-    for playlist in allplaylist:
+            for track in playlist['tracks']:
+                if 'track' in track:
+                    album = encode_utf(track["track"]["album"])
+                    artist = encode_utf(track["track"]["artist"])
+                    title = encode_utf(track["track"]["title"])
+                    verification_query = album + artist + title
 
-        playlist_name = playlist['name'].encode('utf-8').strip()
-        song_list = []
+                    if search_query in verification_query:
+                        song_list.append("{} by {}".format(title, artist))
 
-        for track in playlist['tracks']:
-            if 'track' in track:
-                album = encode_utf(track["track"]["album"])
-                artist = encode_utf(track["track"]["artist"])
-                title = encode_utf(track["track"]["title"])
-                verification_query = album + artist + title
+            if len(song_list) != 0:
+                result[playlist_name] = song_list
 
-                if search_query in verification_query:
-                    song_list.append("{} by {}".format(title, artist))
-
-        if len(song_list) != 0:
-            result[playlist_name] = song_list
-
-    for name in result:
-        print(name)
-        for chanson in result[name]:
-            print("\t - {}".format(chanson))
+        for name in result:
+            print(name)
+            for chanson in result[name]:
+                print("\t - {}".format(chanson))
